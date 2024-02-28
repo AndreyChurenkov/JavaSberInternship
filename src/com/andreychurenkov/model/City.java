9bf2d4527692179cc391b71156304f4d8ad6b0e4
Andrey Churenkov <andrey.churenkov@gmail.com>
2024-02-28 15:13:08 +0300

    add packages, add model City

---
+++ src/com/andreychurenkov/model/City.java
@@ -0,0 +1,57 @@
+package com.andreychurenkov.model;
+
+public class City {
+    String name;
+    String region;
+    String district;
+    int population;
+    String foundation;
+
+    public City(String name, String region, String district, int population, String foundation) {
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getFoundation() {
+        return foundation;
+    }
+
+    public void setFoundation(String foundation) {
+        this.foundation = foundation;
+    }
+}
