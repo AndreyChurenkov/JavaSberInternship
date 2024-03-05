9739b3b745cfc9b2abee4125f0fee517379df045
Andrey Churenkov <andrey.churenkov@gmail.com>
2024-03-05 18:05:57 +0300

    add main

--- src/com/andreychurenkov/Main.java
+++ src/com/andreychurenkov/Main.java
@@ -1,8 +1,20 @@
 package com.andreychurenkov;
 
+import com.andreychurenkov.model.City;
+import com.andreychurenkov.util.CitiesUtil;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<City> cities = CitiesUtil.parseCitiesFromFile();
+        printCities(cities);
+    }
+
+    private static void printCities(List<City> cities) {
+        for (City city : cities) {
+            System.out.println(city + "\n");
+        }
     }
 }
