0819fcbe885381c53cfff1170d3c5b4c587157dc
Andrey Churenkov <andrey.churenkov@gmail.com>
2024-03-05 19:00:52 +0300

    update main

--- src/com/andreychurenkov/Main.java
+++ src/com/andreychurenkov/Main.java
@@ -8,7 +8,27 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        Создаем список городов из файла
+        */
         List<City> cities = CitiesUtil.parseCitiesFromFile();
+        printCities(cities);
+
+        /*
+        Сортировка списка городов по наименованию в алфавитном порядке без учета регистра
+
+        В задании указана сортировка по убыванию, однако в примере по возрастанию, сделал согласно примеру
+        */
+        CitiesUtil.sortByName(cities);
+        printCities(cities);
+
+        /*
+        Сортировка списка городов по федеральному округу и
+        наименованию города внутри каждого федерального округа в алфавитном порядке с учетом регистра
+
+        В задании указана сортировка по убыванию, однако в примере по возрастанию, сделал согласно примеру
+        */
+        CitiesUtil.sortByFederalDistrictAndName(cities);
         printCities(cities);
     }
 
