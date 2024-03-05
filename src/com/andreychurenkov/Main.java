085fccbbc183e538817c0b8faa711c55d093aaa4
Andrey Churenkov <andrey.churenkov@gmail.com>
2024-03-05 20:37:21 +0300

    update main

--- src/com/andreychurenkov/Main.java
+++ src/com/andreychurenkov/Main.java
@@ -3,6 +3,7 @@ package com.andreychurenkov;
 import com.andreychurenkov.model.City;
 import com.andreychurenkov.util.CitiesUtil;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -12,15 +13,14 @@ public class Main {
         Создаем список городов из файла
         */
         List<City> cities = CitiesUtil.parseCitiesFromFile();
-        printCities(cities);
+        printCitiesList(cities);
 
         /*
         Сортировка списка городов по наименованию в алфавитном порядке без учета регистра
 
         В задании указана сортировка по убыванию, однако в примере по возрастанию, сделал согласно примеру
         */
-        CitiesUtil.sortByName(cities);
-        printCities(cities);
+        printCitiesList(CitiesUtil.sortByName(cities));
 
         /*
         Сортировка списка городов по федеральному округу и
@@ -28,13 +28,25 @@ public class Main {
 
         В задании указана сортировка по убыванию, однако в примере по возрастанию, сделал согласно примеру
         */
-        CitiesUtil.sortByFederalDistrictAndName(cities);
-        printCities(cities);
+        printCitiesList(CitiesUtil.sortByFederalDistrictAndName(cities));
+
+        /*
+        Преобразование список городов в массив.
+        Поиск путем перебора массива индекса элемента и значения с наибольшим количеством жителей города
+        */
+        printCitiesArray(CitiesUtil.convertToIndexPopulationArray(cities));
+
     }
 
-    private static void printCities(List<City> cities) {
+    private static void printCitiesList(List<City> cities) {
         for (City city : cities) {
             System.out.println(city);
+        }
+    }
+
+    private static void printCitiesArray(int[][] indexPopulationArray) {
+        for (int[] pair : indexPopulationArray) {
+            System.out.println("[" + pair[0] + "] = " + pair[1]);
         }
     }
 }
